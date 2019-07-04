7b28123b6a3ce01811ecfd4ae0313adc468d3417
stjeong <kevin13@chol.net>
2019-07-04 10:44:45 +0900

    Initial commit

---
+++ server.js
@@ -0,0 +1,51 @@
+
+const express = require("express");
+const hbs = require("hbs");
+const fs = require("fs");
+
+var app = express();
+
+hbs.registerPartials(__dirname + "/views/partials");
+app.set("view engine", "hbs");
+
+app.use((req, res, next) => {
+    var now = new Date().toString();
+    var log = `${now}: ${req.method} ${req.url}`;
+    console.log(log);
+
+    fs.appendFile("server.log", log + "\n", (err) => { });
+    next();
+});
+app.use((req, res, next) => {
+    // res.render("maintenance.hbs");
+    next();
+});
+app.use(express.static(__dirname + "/public"));
+
+hbs.registerHelper("getCurrentYear", () => new Date().getFullYear());
+hbs.registerHelper("screamIt", (text) => text.toUpperCase());
+
+app.get("/", (req, res) => {
+    res.render("home.hbs", {
+        pageTitle: "Home Page",
+        welcomeMessage: "Welcome to my website",
+    });
+});
+
+app.get("/about", (req, res) => {
+    res.render("about.hbs", {
+        pageTitle: "About Page",
+    });
+});
+
+app.get("/bad", (req, res) => {
+    res.send({
+        errorMessage: "Unabled to handle request"
+    });
+});
+
+var server = app.listen(3000, () => {
+    console.log("Server is up on port " + server.address().port);
+});
+
+
